8212d0b8b7382da1f243c40058fc8730e852dd0d
gronerth <tgronerth@gmail.com>
2020-10-25 18:33:25 -0500

    add downstream

--- disableUpstreamChannels.py
+++ disableUpstreamChannels.py
@@ -22,6 +22,7 @@ class docsisChannel(object):
 class docsisChannels(object):
 	def __init__(self):
 		self.upstream_channel = {}
+		self.downstream_channel = {}
 
 	def addUpstreamChannel(self,ifDescr):
 		self.upstream_channel[ifDescr] = docsisChannel(ifDescr)
@@ -33,6 +34,7 @@ parser.add_argument('--ip',dest='ip_address',required=True,help='Hostname or IP 
 parser.add_argument('--olt',dest='olt_name',required=True,help='Name of the OLT')
 parser.add_argument('--olt_file', dest='olt_file_name', default="",
                     help='File with the list of olts in csv format oltname,IP')
+parser.add_argument('--type_channel',dest='type_channel',default="u",help="Export upstream (u), downstream(d) or both (ud)")
 parser.add_argument('--community',dest='community',default='u2000_ro',help='SNMP read community')
 parser.add_argument('--disUpFreq',dest='disUpFreq',default='24.2,19.4,17.8',help='Docsis 3.0 upstream frequencies to disable')
 #parser.add_argument('--measurement',dest='community',default='u2000_ro',help='SNMP read community')
@@ -86,6 +88,53 @@ def pollDocsisChannels(olt_name,ip_address,community):
 		print(olt_name+","+docsis_channels.upstream_channel[upstream_channel].ifDescr + "," +  str(channel_frequency) + "," +  docsis_channels.upstream_channel[upstream_channel].status)
 
 
+def pollDownstreamDocsisChannels(olt_name,ip_address,community):
+  
+	session = Session(hostname=ip_address, community=community, version=2, use_numeric=True)
+
+	docsis_channels = docsisChannels()
+
+	downstream_initial_index=2013798400
+
+	finishLoop=False
+
+	while(finishLoop==False):
+
+		oids=[]
+		oids.append('IF-MIB::ifDescr.' + str(downstream_initial_index))
+		oids.append('IF-MIB::ifAdminStatus.' + str(downstream_initial_index))
+		oids.append('.1.3.6.1.2.1.10.127.1.1.1.1.2.' + str(downstream_initial_index)) #Frequency
+
+		docsis_channel_stats = session.get_bulk(oids,non_repeaters=0,max_repetitions=snmp_max_repetitions)
+
+
+		for item in docsis_channel_stats:
+			downstream_initial_index=int(item.oid_index)
+			if downstream_initial_index >= 4194312192:
+				#print("Finishing loop, upstream_initial_index = " + str(upstream_initial_index))
+				finishLoop=True
+				break
+			if item.oid == '.1.3.6.1.2.1.2.2.1.2':#ifDescr
+				if "docsCableDownstream" in item.value:
+					docsis_channels.downstream_channel[item.oid_index] = docsisChannel(str(item.value))
+					docsis_channels.downstream_channel[item.oid_index].ifDescr = str(item.value) #redundancy?
+			elif item.oid == '.1.3.6.1.2.1.2.2.1.7':#ifAdminStatus
+				if item.oid_index  in docsis_channels.downstream_channel:
+					docsis_channels.downstream_channel[item.oid_index].setStatus(str(item.value))
+			elif '.1.3.6.1.2.1.10.127.1.1.1.1.2' in item.oid:#Frequency
+				#print("upstream_initial_index = " + str(upstream_initial_index))
+				if item.oid_index  in docsis_channels.downstream_channel:
+					docsis_channels.downstream_channel[item.oid_index].setFrequency(int(item.value))
+									
+		
+
+	for downstream_channel in docsis_channels.downstream_channel:
+		channel_frequency = docsis_channels.downstream_channel[downstream_channel].frequency
+		if channel_frequency == 0:
+			continue 
+		print(olt_name+","+docsis_channels.downstream_channel[downstream_channel].ifDescr + "," +  str(channel_frequency) + "," +  docsis_channels.downstream_channel[downstream_channel].status)
+
+
 args = parser.parse_args()
 
 if args.olt_file_name != "":
@@ -98,7 +147,10 @@ if len(olt_list)>0:
 	for olt_name in olt_list:
 		try:
 			#print(olt_name)
-			pollDocsisChannels(olt_name,olt_list[olt_name],args.community)
+			if(args.type_channel=="u"):
+				pollDocsisChannels(olt_name,olt_list[olt_name],args.community)
+			elif args.type_channel=="d":
+				pollDownstreamDocsisChannels(olt_name,olt_list[olt_name],args.community)
 		except Exception as e:
 			print("olt_name: " + olt_name,e)
 			continue
